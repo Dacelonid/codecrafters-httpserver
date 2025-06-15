8ec624f4ad6f263694a6edc29b1a898029207251
Ken O'Neill <kenneth.oneill@gmail.com>
2025-06-15 11:24:00 +0100

    Added builder to HTTPHeader class

--- src/main/java/server/httpUtils/HttpConstants.java
+++ src/main/java/server/httpUtils/HttpConstants.java
@@ -11,17 +11,9 @@ public class HttpConstants {
     public static final String DEFAULT_ENCODING = "identity";
     public static final String DEFAULT_LANGUAGE = "en-US";
     public static final String DEFAULT_CHARSET = "UTF-8";
-    public static final String DEFAULT_CONTENT_TYPE = "text/plain";
-    public static final String DEFAULT_CONTENT_LENGTH = "0";
-    public static final String DEFAULT_CONTENT_ENCODING = "binary";
-    public static final String DEFAULT_CONTENT_LANGUAGE = "en-US";
-    public static final String DEFAULT_CONTENT_CHARSET = "UTF-8";
     public static final String DEFAULT_CONNECTION = "keep-alive";
-    public static final String DEFAULT_UPGRADE = "none";
-    public static final String DEFAULT_TRAILER = "none";
-    public static final String DEFAULT_VIA = "1.1 (codecrafters-http-server-java)";
-    public static final String DEFAULT_CACHE_CONTROL = "no-cache";
-    public static final String DEFAULT_PRAGMA = "no-cache";
+    public static final String DEFAULT_ACCEPT = "*/*";
+    public static final String DEFAULT_EMPTY = "";
 
 
 
--- src/main/java/server/httpstructure/httprequest/HttpHeader.java
+++ src/main/java/server/httpstructure/httprequest/HttpHeader.java
@@ -1,35 +1,41 @@
 package server.httpstructure.httprequest;
 
+import static server.httpUtils.HttpConstants.*;
+
 public class HttpHeader {
     private final ClientInfo clientInfo;
     private final GeneralInfo generalInfo;
     private final ContentNegotiation contentNegotiationInfo;
 
-    private HttpHeader(String userAgent, String host, String accept, String acceptEncoding, String connection) {
-        this.clientInfo = new ClientInfo(userAgent, null, null, null);
-        this.generalInfo = new GeneralInfo(host, connection);
-        this.contentNegotiationInfo = new ContentNegotiation(accept, acceptEncoding);
+    private HttpHeader(HttpHeaderBuilder builder) {
+        this.clientInfo = new ClientInfo(builder.userAgent, builder.referer, builder.from, builder.xRequestedWith);
+        this.generalInfo = new GeneralInfo(builder.host, builder.connection, builder.upgrade, builder.via, builder.cacheControl, builder.pragma, builder.trailer);
+        this.contentNegotiationInfo = new ContentNegotiation(
+                builder.accept,
+                builder.acceptEncoding,
+                builder.acceptLanguage,
+                builder.acceptCharset
+        );
+    }
+
+    public static HttpHeaderBuilder builder() {
+        return new HttpHeaderBuilder();
     }
 
     public static HttpHeader build(String[] headers) {
-        String userAgent = "";
-        String host = "";
-        String accept = "";
-        String acceptEncoding = "";
-        String connection = "";
+        HttpHeaderBuilder builder = builder();
         for (String header : headers) {
             if (header.startsWith("Host")) {
-                host = header.substring(5).trim();
+                builder = builder.host(header.substring(5).trim());
             }
             if (header.startsWith("User-Agent")) {
-                userAgent = header.substring(11).trim();
+                builder = builder.userAgent(header.substring(11).trim());
             }
             if (header.startsWith("Accept-Encoding")) {
-                acceptEncoding = header.substring(15).trim();
+                builder = builder.acceptEncoding(header.substring(15).trim());
             }
         }
-
-        return new HttpHeader(userAgent, host, accept, acceptEncoding, connection);
+        return builder.build();
     }
 
     public String getUserAgent() {
@@ -38,5 +44,110 @@ public class HttpHeader {
 
     public String getHost() {
         return generalInfo.host();
+    }
+
+    public String getAccept() {
+        return contentNegotiationInfo.accept();
+    }
+
+
+    // === Builder Class ===
+    public static class HttpHeaderBuilder {
+        private String upgrade = DEFAULT_EMPTY;
+        private String via = DEFAULT_EMPTY;
+        private String cacheControl = DEFAULT_EMPTY;
+        private String pragma = DEFAULT_EMPTY;
+        private String trailer = DEFAULT_EMPTY;
+        private String referer = DEFAULT_EMPTY;
+        private String from = DEFAULT_EMPTY;
+        private String xRequestedWith = DEFAULT_EMPTY;
+
+        private String userAgent = DEFAULT_EMPTY;
+        private String host = DEFAULT_EMPTY;
+        private String connection = DEFAULT_CONNECTION;
+
+        private String accept = DEFAULT_ACCEPT;
+        private String acceptEncoding = DEFAULT_ENCODING;
+        private String acceptLanguage = DEFAULT_LANGUAGE;
+        private String acceptCharset = DEFAULT_CHARSET;
+
+        public HttpHeaderBuilder upgrade(String upgrade) {
+            this.upgrade = upgrade;
+            return this;
+        }
+
+        public HttpHeaderBuilder via(String via) {
+            this.via = via;
+            return this;
+        }
+
+        public HttpHeaderBuilder cacheControl(String cacheControl) {
+            this.cacheControl = cacheControl;
+            return this;
+        }
+
+        public HttpHeaderBuilder pragma(String pragma) {
+            this.pragma = pragma;
+            return this;
+        }
+
+        public HttpHeaderBuilder trailer(String trailer) {
+            this.trailer = trailer;
+            return this;
+        }
+
+        public HttpHeaderBuilder referer(String referer) {
+            this.referer = referer;
+            return this;
+        }
+
+        public HttpHeaderBuilder from(String from) {
+            this.from = from;
+            return this;
+        }
+
+        public HttpHeaderBuilder xRequestedWith(String xRequestedWith) {
+            this.xRequestedWith = xRequestedWith;
+            return this;
+        }
+
+        public HttpHeaderBuilder userAgent(String userAgent) {
+            this.userAgent = userAgent;
+            return this;
+        }
+
+        public HttpHeaderBuilder host(String host) {
+            this.host = host;
+            return this;
+        }
+
+        public HttpHeaderBuilder connection(String connection) {
+            this.connection = connection;
+            return this;
+        }
+
+        public HttpHeaderBuilder accept(String accept) {
+            this.accept = accept;
+            return this;
+        }
+
+        public HttpHeaderBuilder acceptEncoding(String acceptEncoding) {
+            this.acceptEncoding = acceptEncoding;
+            return this;
+        }
+
+        public HttpHeaderBuilder acceptLanguage(String acceptLanguage) {
+            this.acceptLanguage = acceptLanguage;
+            return this;
+        }
+
+        public HttpHeaderBuilder acceptCharset(String acceptCharset) {
+            this.acceptCharset = acceptCharset;
+            return this;
+        }
+
+        public HttpHeader build() {
+            return new HttpHeader(this);
+        }
     }
 }
