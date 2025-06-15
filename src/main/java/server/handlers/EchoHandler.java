6ae2f36896fe88ac8362415ace31af46d608bc80
Ken O'Neill <kenneth.oneill@gmail.com>
2025-06-15 13:11:50 +0100

    Added support for checking multiple accept content

--- src/main/java/server/handlers/EchoHandler.java
+++ src/main/java/server/handlers/EchoHandler.java
@@ -4,12 +4,20 @@ import server.httpUtils.HttpCodes;
 import server.httpstructure.httprequest.HttpRequest;
 import server.httpstructure.httpresponse.HttpResponse;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class EchoHandler implements Handler{
     public HttpResponse handle(HttpRequest request) {
         String path = request.getTarget().substring("/echo/".length());
-        if(request.getHttpHeader().getAcceptEncoding().equals("gzip")){
+        if(isGzip(request)){
             return HttpResponse.builder().responseCode(HttpCodes.OK).contentType("text/plain").contentEncoding("gzip").body(path).build();
         }
         return HttpResponse.builder().responseCode(HttpCodes.OK).contentType("text/plain").body(path).build();
+    }
+
+    private static boolean isGzip(HttpRequest request) {
+        List<String> acceptEncoding = Arrays.asList(request.getHttpHeader().getAcceptEncoding().split(", "));
+        return acceptEncoding.contains("gzip");
     }
 }
